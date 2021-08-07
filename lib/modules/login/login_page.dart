5d41358b28f78722aa73a5cc603d3483a85aac71
Francisco Gaieski <>
2021-08-06 22:21:10 -0300

    feat: added fonts and colors to login page

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:split_it/theme/app_theme.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -11,30 +13,75 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppTheme.colors.backgroundPrimary,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Text("Divida contas com seus amigos"),
+          Padding(
+            padding: const EdgeInsets.only(left: 40),
+            child: Row(
+              children: [
+                Container(
+                  width: 236,
+                  child: Text(
+                    "Divida contas com seus amigos",
+                    style: GoogleFonts.montserrat(
+                        fontSize: 40,
+                        fontWeight: FontWeight.w700,
+                        color: AppTheme.colors.title),
+                  ),
+                ),
+              ],
+            ),
+          ),
           Column(
             children: [
               ListTile(
-                leading: Icon(Icons.sim_card),
-                title: Text("Faça seu login com as contas abaixo"),
+                leading: Text(
+                  "😍",
+                  style: GoogleFonts.inter(
+                      fontSize: 40, fontWeight: FontWeight.w400),
+                ),
+                title: Text(
+                  "Faça seu login com as contas abaixo",
+                  style: GoogleFonts.inter(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                      color: AppTheme.colors.button),
+                ),
               ),
               SizedBox(
                 height: 32,
               ),
               ElevatedButton.icon(
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(
+                          AppTheme.colors.backgroundPrimary)),
                   onPressed: () {},
-                  icon: Icon(Icons.email),
-                  label: Text("Entrat com Google")),
+                  icon: Image.asset("assets/images/google-icon-1.png"),
+                  label: Text(
+                    "Entrar com Google",
+                    style: GoogleFonts.inter(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                        color: AppTheme.colors.button),
+                  )),
               SizedBox(
                 height: 12,
               ),
               ElevatedButton.icon(
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(
+                          AppTheme.colors.backgroundPrimary)),
                   onPressed: () {},
-                  icon: Icon(Icons.email),
-                  label: Text("Entrat com Apple"))
+                  icon: Image.asset("assets/images/apple-1.png"),
+                  label: Text(
+                    "Entrar com Apple",
+                    style: GoogleFonts.inter(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                        color: AppTheme.colors.button),
+                  ))
             ],
           ),
         ],
--- lib/theme/app_colors.dart
+++ lib/theme/app_colors.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 
 abstract class AppColorsBase {
-  Color get background;
+  Color get backgroundSecondary;
+  Color get backgroundPrimary;
+  Color get title;
+  Color get button;
 }
 
 class AppColorsDefault implements AppColorsBase {
   @override
-  Color get background => Color(0xFF40B38C);
+  Color get backgroundPrimary => Color(0xFFFFFFFF);
+
+  @override
+  Color get backgroundSecondary => Color(0xFF40B38C);
+
+  @override
+  Color get title => Color(0xFF40B28C);
+
+  @override
+  Color get button => Color(0xFF666666);
 }
