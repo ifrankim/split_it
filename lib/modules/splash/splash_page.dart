a7d61e1be7576fdfb29bc59782334bdcc8ca8f4f
Francisco Gaieski <>
2021-08-11 17:02:31 -0300

    feat: installed and configured Firebase

--- lib/modules/splash/splash_page.dart
+++ lib/modules/splash/splash_page.dart
@@ -1,9 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:split_it/theme/app_gradients.dart';
 import 'package:split_it/theme/app_theme.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  void initializeFirebase() async {
+    try {
+      await Firebase.initializeApp();
+    } catch (e) {}
+  }
+
+  @override
+  void initState() {
+    initializeFirebase();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
