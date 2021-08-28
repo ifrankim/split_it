11f5ac5c25b9038ee11e63c918c697d5d1b4434f
Francisco Gaieski <>
2021-08-27 22:17:52 -0300

    feat(login_page): added TODO of Apple Signin

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 import 'package:split_it/modules/login/widgets/social_button.dart';
 import 'package:split_it/theme/app_theme.dart';
 
@@ -72,10 +73,26 @@ class _LoginPageState extends State<LoginPage> {
                   height: 12,
                 ),
                 SocialButton(
-                  imagePath: "assets/images/apple-1.png",
-                  label: "Entrar com Apple",
-                  onTap: () {},
-                ),
+                    imagePath: "assets/images/apple-1.png",
+                    label: "Entrar com Apple",
+                    onTap: () async {
+                      //TODO: Fazer configurações da AppStore
+                      try {
+                        final credential =
+                            await SignInWithApple.getAppleIDCredential(
+                          scopes: [
+                            AppleIDAuthorizationScopes.email,
+                            AppleIDAuthorizationScopes.fullName,
+                          ],
+                        );
+                        print(credential);
+
+                        // Now send the credential (especially `credential.authorizationCode`) to your server to create a session
+                        // after they have been validated with Apple (see `Integration` section for more information on how to do this)
+                      } catch (e) {
+                        print(e);
+                      }
+                    }),
               ],
             ),
           ],
