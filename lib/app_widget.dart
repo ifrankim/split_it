318c735c37858d000bd7b13a247b7fbd7899afc7
Francisco Gaieski <>
2021-09-13 21:34:54 -0300

    feat(home): added homscreen file

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:split_it/modules/error/error_page.dart';
+import 'package:split_it/modules/home/home_page.dart';
 import 'package:split_it/modules/login/login_page.dart';
 import 'package:split_it/modules/splash/splash_page.dart';
 
@@ -14,7 +15,8 @@ class AppWidget extends StatelessWidget {
       routes: {
         "/splash": (context) => SplashPage(),
         "/login": (context) => LoginPage(),
-        "/error": (context) => ErrorPage()
+        "/error": (context) => ErrorPage(),
+        "/home": (context) => HomePage(),
       },
     );
   }
